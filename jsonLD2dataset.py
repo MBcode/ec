c2d2f480a2b65ec564ded71fdf4dd6abacd6822f
Mike Bobak <mbobak@illinois.edu>
2020-09-30 12:10:31 -0500

    run_all path based

--- jsonLD2dataset.py
+++ jsonLD2dataset.py
@@ -1,6 +1,7 @@
 #Get my cached repo metadata asserted into a clowder dataset stub, to describe a repo's dataset-laninding-page ;mbobak
 import requests
 import json
+import os
 clowder_host = "https://earthcube.clowderframework.org"
 clowder_key = os.getenv('eckey')
 clowder_space = os.getenv('clowder_space') #change per repo
@@ -8,7 +9,7 @@ print(clowder_space)
 #make sure the input fn's have been milled w/something like: #only iedadata, rest find w/encode utf-8
 # alias dcat2i "sed -i '/http:..www.w3.org.ns.dcat#/s//dcat:/g'"
 
-#don't need strip js for internal txt, but want bs-stripping so just use this1for now
+#don't need strip js for internal txt, but want bs4-stripping so just use this one for now
 def stripjshtm(html):
     from bs4 import BeautifulSoup
     import re
@@ -18,9 +19,8 @@ def stripjshtm(html):
     text = soup.get_text()
     return re.sub(r'(\n\s*)+\n+', '\n', text.strip())
 
-def jsonLD2dataset(fn):
+def jsonLD2dataset(path):
     "create clowder dataset w/title,abstract,url and assert assoc jsonLD"
-    path = './ld/' + fn
     print(path)
     with open(path) as f:
         jsLD=f.read().encode("utf-8")
@@ -34,12 +34,12 @@ def jsonLD2dataset(fn):
         name = d['spatialCoverage']['name']
     if name:
         name=stripjshtm(name)
+    #so find a way2only run for it, maybe loading a schema based cfg for nonstandard location
     #url = d['identifier'][0]['url'] #iedadata
-    #idurl = d['identifier']
-    idurl = d['identifier'][0]
+    idurl = d.get('identifier')
     if idurl:
-        url=idurl.get('url')
-    if not url: #for most repos 
+        url=idurl[0].get('url')
+    else: #if not url: #for most repos 
         url = d.get('url')
     #if not url: #should only run this if know in that repo
     #    fnb = fn.split('.')[0]
@@ -70,7 +70,7 @@ def jsonLD2dataset(fn):
     os.system(f"mv -i {path} ldone")
     return datasetID
 
-#jsonLD2dataset('310068.jsonld')
+#jsonLD2dataset('ld/310068.jsonld')
 
 #Then to check it was uploaded, or when get search results, &want metadata, send an 'id' to this:
 def getLD(datasetID):
@@ -78,12 +78,13 @@ def getLD(datasetID):
     r = requests.get(f'{clowder_host}/api/datasets/{datasetID}/metadata', headers={'X-API-Key' : clowder_key})
     print(json.dumps(r.json(), indent=2))
 
-#have a fnc to ls all the jsonld in a dir, and map over that: list(map(jsonLD2dataset,os.listdir('ld')))
+#have a fnc to ls all the jsonld in a dir, and map over that: 
+# started w/: list(map(jsonLD2dataset,os.listdir('ld')))
+# but path based now
 def run_all():
-    #import os
-    #files=os.listdir('ld')
     import glob
-    files=glob.glob('./ld/*.jsonld')
-    list(map(jsonLD2dataset,files))
+    paths=glob.glob('./ld/*.jsonld') #this gives path, not fns so change above
+    list(map(jsonLD2dataset,paths))
+
 # maybe returning a file w/the filename to datasetID mappings
-#jsonLD2dataset('609656.jsonld')
+#jsonLD2dataset('ld/609656.jsonld')
