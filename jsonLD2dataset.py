46ce665c41c270fbe0bb7e2b9c1e90e43ce94356
Mike Bobak <mbobak@illinois.edu>
2020-10-14 21:15:00 -0500

    can assert ResourceRegistry metadata

--- jsonLD2dataset.py
+++ jsonLD2dataset.py
@@ -9,6 +9,18 @@ print(clowder_space)
 #make sure the input fn's have been milled w/something like: #only iedadata, rest find w/encode utf-8
 # alias dcat2i "sed -i '/http:..www.w3.org.ns.dcat#/s//dcat:/g'"
 
+def first(l):
+    if isinstance(l,list):
+        return l[0]
+    else:
+        return l
+
+def httpP(str):
+    str.startswith('http')
+
+def getURLs(l):
+    filter(httpP,l)
+
 #don't need strip js for internal txt, but want bs4-stripping so just use this one for now
 def stripjshtm(html):
     from bs4 import BeautifulSoup
@@ -18,7 +30,8 @@ def stripjshtm(html):
         script.extract()
     text_wtags = soup.get_text()
     text = BeautifulSoup(text_wtags, "lxml").text #rm more tags here
-    return re.sub(r'(\n\s*)+\n+', '\n', text.strip())
+    ret = re.sub(r'(\n\s*)+\n+', '\n', text.strip())
+    return first(ret) #not needed
 
 
 def dict2dataset(d,url=None):
@@ -30,9 +43,11 @@ def dict2dataset(d,url=None):
         name=stripjshtm(name)
     else:
         name = " "
+    print(name)
     #so find a way2only run for it, maybe loading a schema based cfg for nonstandard location
     #url = d['identifier'][0]['url'] #iedadata
 #   idurl = d.get('identifier')
+##  idurl = first(d.get('identifier'))
 #   if idurl:
 #       if isinstance(idurl,list):
 #           url=idurl[0].get('url')
@@ -42,37 +57,65 @@ def dict2dataset(d,url=None):
 #       url = d.get('url')
     if not url:
         url = d.get('url')
+    if not url: #for RR
+        so = d.get('subjectOf')
+        if so:
+           #print(len(so))
+           #print(type(so))
+           #print(so)
+            #so = first(so)
+            if isinstance(so,dict):
+                url = so.get('URL')
+            if isinstance(so,list):
+                url = so[0].get('URL')
+    if not url: #for RR
+        url = d.get('@id')
+    if not url:
+        url = " "
+    if isinstance(url,list):
+        print(url)
+        #url=first(getURLs(url))
+        url=getURLs(url)
+        print(url)
 
     #if not url: #should only run this if know in that repo
     #    fnb = fn.split('.')[0]
     #    url = "http://data.neotomadb.org/datasets/" + fnb
     #    print(url)
     des = d.get('description')
+    if isinstance(des,list):
+        des=des[0]
+    print(des)
     if url:
         if des:
-            du = stripjshtm(des) + " " + url
+            sdes = stripjshtm(des) 
+           #print(sdes)
+           #print(url)
+            du = sdes + " " + url
+            #du = stripjshtm(des) + " " + url
         else:
             du = name  + " " + url
         metadata3={'name': name, 'description': du, 'url': url, 'space': [clowder_space], 'access': 'PUBLIC'}
     else:
-        metadata3={'name': name, 'description': du, 'space': [clowder_space], 'access': 'PUBLIC'}
+        #metadata3={'name': name, 'description': du, 'space': [clowder_space], 'access': 'PUBLIC'}
+        metadata3={'name': name, 'description': des, 'space': [clowder_space], 'access': 'PUBLIC'}
         #metadata3={'name': name, 'description': des} 
     print(metadata3)
     headers3={'X-API-Key': clowder_key,'accept': '*/*', 'Content-Type': 'application/json'}
     print(headers3)
-    rds=requests.post(f'{clowder_host}/api/datasets/createempty',headers=headers3, data=json.dumps(metadata3))
-    print(rds.json()) #check for good return before running the rest 
-    datasetID=rds.json()['id'] 
-    print(datasetID)
+#   rds=requests.post(f'{clowder_host}/api/datasets/createempty',headers=headers3, data=json.dumps(metadata3))
+#   print(rds.json()) #check for good return before running the rest 
+#   datasetID=rds.json()['id'] 
+#   print(datasetID)
     headers2={'X-API-Key': clowder_key, 'Content-Type': 'application/json'}
-    URL=f'{clowder_host}/api/datasets/{datasetID}/metadata'
-    print(URL)
-    r=requests.post(URL,data=jsLD,headers=headers2)
-    print(r.json())
-    ##os.system(f"mv {path} ldone")
-    #os.system(f"mv -i {path} ldone")
-    print(datasetID)
-    return datasetID
+#   URL=f'{clowder_host}/api/datasets/{datasetID}/metadata'
+#   print(URL)
+#    r=requests.post(URL,data=jsLD,headers=headers2)
+#    print(r.json())
+#   print(datasetID)
+#   return datasetID
+    #return "test"
+    return metadata3
 
 def jsonLD2dataset(path):
     "create clowder dataset w/title,abstract,url and assert assoc jsonLD"
@@ -122,6 +165,13 @@ def getjsonLD(datasetID):
     r = requests.get(f'{clowder_host}/api/datasets/{datasetID}/metadata.jsonld', headers={'X-API-Key' : clowder_key})
     print(json.dumps(r.json(), indent=2))
 
+#logging
+def log_dsIDs(rl):
+    with open('dsIDs.txt','w') as f:
+        for item in rl:
+            f.write("%s\n" % item)
+#w/bu reconstruction from fn's could pair w/clowder IDs
+
 #have a fnc to ls all the jsonld in a dir, and map over that: 
 # started w/: list(map(jsonLD2dataset,os.listdir('ld')))
 # but path based now
@@ -133,11 +183,16 @@ def run_all_ld():
 def run_all_js():
     import glob
     paths=glob.glob('./js/*.js') #this gives path, not fns so change above
-    #return list(map(jsLD2dataset,paths))
     rl= list(map(jsLD2dataset,paths))
-    with open('dsIDs.txt','w') as f:
-        for item in rl:
-            f.write("%s\n" % item)
+    log_dsIDs(rl)
+    return rl
+
+def run_all_json():
+    import glob
+    #bu=os.getenv('BASE_URL') #might use w/end of paths to get url,last resort
+    paths=glob.glob('./json/*.json') #this gives path, not fns so change above
+    rl = list(map(jsonLD2dataset,paths))
+    log_dsIDs(rl)
     return rl
 
 # maybe returning a file w/the filename to datasetID mappings
