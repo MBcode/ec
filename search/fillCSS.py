1f744d4e99586a379bb49de434689e3041b6a7e2
Mike Bobak <mbobak@illinois.edu>
2020-11-06 09:58:18 -0600

    more div-s

--- search/fillCSS.py
+++ search/fillCSS.py
@@ -64,6 +64,39 @@ def cj2h(j):
 #if I ran carrot2 dcs clustering, could just add keyword like links to the clusters that each is in
  #could generate a page for each of these clusters, that is just a filtering of these returns w/in grp
 
+#some styling from: geodexui> view geodex/website/search.html
+#contain = """ <div class="container"><vid class="row"><div class="col-12 center">  .."""
+body = """<body> <div class="body-content" style="padding:0px">"""
+search = """
+   <div style="padding:0px;top:0px" class="container">
+      <div class="row">
+        <div class="col-12 center">
+          <img src="../img/search.svg" style="height:25px">
+          <input style="font-size:120%" type="search" id="q" name="q" aria-label="Search for samples">
+          <button style="color:black;border-color:black;border-style:solid" id="update">Search</button>
+          <!--   <div class="img img-website-mock"></div>  -->
+          <input id="nn" type="number" min="5" max="200" step="5" value="20" />
+  <div id="container1"></div>
+
+        </div>
+      </div>
+    </div>
+"""
+contain2s = """
+    <div style="padding:0px;top:0px" class="container">
+      <div class="row">
+        <div class="col-12">
+          <div id="container2"></div>
+           <div style="margin:30px">
+           """
+contain2e = """
+           </div>
+        </div>
+      </div>
+    </div> 
+    """
+
+
 #could call this externally from one of the flask routes, via: python3 fillCSS.py querystr 
 rj=cq(qry_str)
 #print(rj)
@@ -73,5 +106,10 @@ print("<script>")
 print(json.dumps(rj, indent=2))
 print("</script>")
     #might need something around the table
+#print("<body>")
+print(body)
+print(contain2s)
 cj2h(rj)
+print(contain2e)
+print("</body>")
 print("</html>")
