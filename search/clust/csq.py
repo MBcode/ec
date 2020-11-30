a870ff8ec7f00185c62883957a69d9c199982839
Mike Bobak <mbobak@illinois.edu>
2020-11-29 22:31:28 -0600

    cluster2json

--- search/clust/csq.py
+++ search/clust/csq.py
@@ -1,12 +1,16 @@
 #this supercedes csq.sh, but still uses sq2.py to do the search and cluster it
-import xmltodict #might prefer to have curl below dump in json format
+#import xmltodict #prefer to have curl below dump in json format
+import json
 import sys
 import os
 if(len(sys.argv)>1):
     qry_str=sys.argv[1]
 else:
     qry_str = "organic"
-cs=f'/usr/bin/python3 sq2.py {qry_str} | curl $dcs_url -F "dcs.c2stream=@-"'
+cs=f'/usr/bin/python3 sq2.py {qry_str} | curl $dcs_url -F "dcs.output.format=JSON" -F "dcs.c2stream=@-"'
 s=os.popen(cs).read()
-d=xmltodict.parse(s)
-print(d)
+#d=xmltodict.parse(s)
+d=json.loads(s)
+#print(json.dumps(s, indent=2))
+print(s)
+#print(type(d))
--- search/clust/csq.sh
+++ search/clust/csq.sh
@@ -1,3 +1,2 @@
-#python3 sq.py carbon | curl $dcs_url -F "dcs.c2stream=@-"
-#python3 sq.py $1 | curl $dcs_url -F "dcs.c2stream=@-"
-python3 sq2.py $1 | curl $dcs_url -F "dcs.c2stream=@-"
+#/usr/bin/python3 sq2.py $1 | curl $dcs_url -F "dcs.c2stream=@-"
+/usr/bin/python3 sq2.py $1 | curl $dcs_url -F "dcs.output.format=JSON"  -F "dcs.c2stream=@-"
