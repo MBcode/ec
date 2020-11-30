3260c150eee9f093d5de0706eff9f39212935acf
Mike Bobak <mbobak@illinois.edu>
2020-11-29 22:12:55 -0600

    cluster search2py dict

---
+++ search/clust/csq.py
@@ -0,0 +1,12 @@
+#this supercedes csq.sh, but still uses sq2.py to do the search and cluster it
+import xmltodict #might prefer to have curl below dump in json format
+import sys
+import os
+if(len(sys.argv)>1):
+    qry_str=sys.argv[1]
+else:
+    qry_str = "organic"
+cs=f'/usr/bin/python3 sq2.py {qry_str} | curl $dcs_url -F "dcs.c2stream=@-"'
+s=os.popen(cs).read()
+d=xmltodict.parse(s)
+print(d)
