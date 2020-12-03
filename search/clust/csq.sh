580da2ce7296712c50a3ab3193549608508f48dc
m bobak <mbobak@illinois.edu>
2020-12-02 22:15:37 -0600

    got pub/time/plc back

--- search/clust/csq.sh
+++ search/clust/csq.sh
@@ -1,2 +1,2 @@
 #/usr/bin/python3 sq2.py $1 | curl $dcs_url -F "dcs.c2stream=@-"
-/usr/bin/python3 sq2.py $1 | curl $dcs_url -F "dcs.output.format=JSON"  -F "dcs.c2stream=@-"
+python3 sq2.py $1 | curl $dcs_url -F "dcs.output.format=JSON"  -F "dcs.c2stream=@-"
