09bf4b51ff5e60b4ec0204e48a5b77edce2ca15c
m bobak <mbobak@illinois.edu>
2020-12-02 20:40:23 -0600

    RE txt url fix

--- search/clust/sq2.py
+++ search/clust/sq2.py
@@ -1,3 +1,4 @@
+#text search of a SPARQL endpoint
 #text search of a SPARQL endpoint or clowder, starting w/the one w/more data for now &falling back2the other when needed
  #output ready for carrot2 clustering
 
@@ -14,7 +15,7 @@ else:
     qry_str = "organic"
 
 def qs2dcs(qry_str):
-    "query sparql to-dcs format"
+    "query clowder to-dcs format"
     from SPARQLWrapper import SPARQLWrapper, JSON
     endpoint = os.getenv('dev_endpoint')
     sparql = SPARQLWrapper(endpoint)
@@ -26,10 +27,9 @@ def qs2dcs(qry_str):
     #    FILTER regex(?o, "nitrogen", "i").  }"""
     #qry_str = "carbon"
     q1 = "PREFIX schema: <http://schema.org/> \nselect distinct ?s ?o where {\n"
-#   q2 = "{ ?s schema:description ?o .} UNION \n { ?s schema:keywords ?o .} UNION  \n{ ?s schema:name ?o .} \n  FILTER regex(?o,\""
-    #we don't have to union them, can just: ?s schema:description|schema:keywords|schema:name ?o
-    q2 = "{ ?s schema:description|schema:keywords|schema:name schema:name ?o .}  FILTER regex(?o,\""
-    #used like this also should work as an optional, bc it's any of these relations that match, for the free-txt-search
+    q2 = "{ ?s schema:description ?o .} UNION \n { ?s schema:keywords ?o .} UNION  \n{ ?s schema:name ?o .} \n  FILTER regex(?o,\""
+    #q2 = "{ ?s schema:description|schema:keywords|schema:name schema:name ?o .}  FILTER regex(?o,\""
+    #before fuseki & then this blazegraph instance I also had a cache of HDT files, so have code w/:
     q4 = "\", \"i\"). \n }"
     q = q1 + q2 + qry_str + q4
     #print(q)
@@ -50,13 +50,29 @@ def qs2dcs(qry_str):
         print(f'<snippet>{description}</snippet></document>')
         i=i+1
     print("</searchresult>")
+    #presently xml ready for carrot2 clustering, but will make that an output format for /cluster route
+    # and will get a uniform output as say geodex puts out, though final html is ok too
 
-#presently xml ready for carro2 clustering, but will make that an output format for /cluster route
-# and will get a uniform output as say geodex puts out, though final html is ok too
-
-#before fuseki & then this blazegraph instance I also had a cache of HDT files, so have code w/:
-#from hdt import HDTDocument ;which when worked up allows for same breadth & could take the per repo
-# checkbox info (w/o having to make subgraphs, or putting restriction in sparql query)for other stores
+    #before fuseki & then this blazegraph instance I also had a cache of HDT files, so have code w/:
+    #from hdt import HDTDocument ;which when worked up allows for same breadth & could take the per repo
+    # checkbox info (w/o having to make subgraphs, or putting restriction in sparql query)for other stores
+#-
+def first_(l):
+    from collections.abc import Iterable
+    if isinstance(l,list):
+        #return l[0]
+        if len(l)>0:
+            return list(l)[0]
+        else:
+            return l
+    elif isinstance(l,Iterable):
+        #return list(l)[0]
+        if len(l)>0:
+            return list(l)[0]
+        else:
+            return l
+    else:
+        return l
 #-
 def first(l):
     from collections.abc import Iterable
@@ -75,7 +91,13 @@ def getURLs(l):
     if isinstance(l,str):
         l=l.split()
     #filter(httpP,l)
-    return filter(httpP,l)
+    #return filter(httpP,l)
+    #return list(filter(httpP,l))
+    ret= list(filter(httpP,l))
+    if len(ret)<1:
+        return ""
+    else:
+        return ret
 #-
 def cq(qry_str):
     "clowder search query"
@@ -97,8 +119,6 @@ def cq(qry_str):
 def qc2dcs(qry_str):
     "query clowder to-dcs format"
     rj=cq(qry_str)
-    if isinstance(rj,int): #use sparql endpnt as a backup if clowder down
-        return qs2dcs(qry_str)
 
     cout = """<?xml version="1.0" encoding="UTF-8"?>
         <searchresult>"""
@@ -106,17 +126,19 @@ def qc2dcs(qry_str):
     print(cout,xml_qry)
     i=0
     for r in rj:
-        cid=r['id'] #clowder specific dataset-id
-        name=r['name'].replace("<"," lt ")
-        des=r['description']
-        description=des.replace("<"," lt ")
-        url=first(getURLs(description))
-        #url2= clowder_host + '/datasets/' + r['id']  #this along w/jsonLD can be gotton from cID
-        #print(f'<document id=\"{i}\">')
-        print(f'<document id=\"{cid}\">') #see if id is more flexible
+        #name=r['name']
+        name=r['name'].replace("&","_and_").replace("<"," _lt_ ")
+        #description=r['description']
+        description=r['description'].replace("&","_and_").replace("<"," _lt_ ")
+        try:
+            #url=first(getURLs(description))
+            url=first_(getURLs(description))
+        except:
+            #print(f'dbg:{description}')
+            url=""
+        #url2= clowder_host + '/datasets/' + r['id']
+        print(f'<document id=\"{i}\">')
         print(f'<url>{url}</url>')
-        #print(f'<cid>{cid}</cid>')
-        #print(f'<name>{name}</name>')
         #print(f'<snippet>{description}</snippet></document>')
         print(f'<snippet>{name}')
         print(f'{description}</snippet></document>')
@@ -127,4 +149,3 @@ def qc2dcs(qry_str):
 qc2dcs(qry_str)
 
 #would be cool if could call dcs clustering service from here, but sending via shell file is ok too
-#something like sc2.py could call csq.sh now, but will needed conversion to html&metadata elts added
