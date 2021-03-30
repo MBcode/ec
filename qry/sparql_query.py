e60c19c81a652c25c656fa1439cf98e1f88cb323
m bobak <mbobak@illinois.edu>
2021-03-30 03:41:11 +0000

    save binding2a file

--- qry/sparql_query.py
+++ qry/sparql_query.py
@@ -24,11 +24,32 @@ def get_txtfile(fn):
     with open(fn, "r") as f:
         return f.read()
 
+
 def put_txtfile(fn,s):
     with open(fn, "w") as f:
         return f.write(s)
 
+def get_jsonfile(fn):
+    s=get_txtfile(fn)
+    d=json.loads(s)
+    return d
+
+def put_jsonfile(fn,d):
+    s=json.dumps(d, indent=2)
+    with open(fn, "w") as of:
+        of.write(s) 
+
+def get_json_file(fnb):
+    fn=fnb.replace(' ','+')
+    return get_jsonfile(fn)
+
+def put_json_file(fnb,d):
+    fn=fnb.replace(' ','+')
+    put_jsonfile(fn,d)
+
+
 def qs2b(qry_str):
+    "qry_str to bindings"
     endpoint = "https://graph.geodex.org/blazegraph/namespace/nabu/sparql"
  #  endpoint = os.getenv('dev_endpoint')
     sparql = SPARQLWrapper(endpoint)
@@ -39,8 +60,16 @@ def qs2b(qry_str):
     results = sparql.query().convert()
     #print(f'sq2b:{results}')
     bindings= results["results"]["bindings"]
+    lb=len(bindings)
+    print(f'ret:{lb}')
     print(f'ret:{bindings}')
     return bindings
 
+def qs2bf(qry_str):
+    b=qs2b(qry_str)
+    put_json_file(qry_str + ".json", b)
+
 def tq():
-    qs2b("Norway")
+    #qs2b("Norway")
+    #ret:107
+    qs2bf("Norway")
