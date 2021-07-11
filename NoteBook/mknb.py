121b1478fc936a8e1e514b68b123bb25b24f0175
Mike Bobak <mbobak@illinois.edu>
2021-07-11 13:33:15 -0500

    post_gist fnc

--- NoteBook/mknb.py
+++ NoteBook/mknb.py
@@ -1,13 +1,13 @@
 #start to prototype the code to create a NB, that will become a gist; that uses a template but inserts the url to open
 # then the UI will open the url for this file, and it won't have to be taken from the nb-url in the nb
 # and if it is a gist, can be opened in colab directly; rob liked a rendered gist after suggesting nbpreview
-#url = "http://example.com/file.csv"
-dwnurl  = "https://darchive.mblwhoilibrary.org/bitstream/1912/23805/1/dataset-753388_hhq-chlorophyll__v1.tsv"
-dwnurl="https://darchive.mblwhoilibrary.org/bitstream/1912/26532/1/dataset-752737_bergen-mesohux-2017-dissolved-nutrients__v1.tsv"
+##url = "http://example.com/file.csv"
+#dwnurl  = "https://darchive.mblwhoilibrary.org/bitstream/1912/23805/1/dataset-753388_hhq-chlorophyll__v1.tsv"
+#dwnurl="https://darchive.mblwhoilibrary.org/bitstream/1912/26532/1/dataset-752737_bergen-mesohux-2017-dissolved-nutrients__v1.tsv"
     #now turn this into a flask service that take the dwnurl
-fn = dwnurl.replace("/","_").replace(":__","/",1) + ".ipynb"
+#fn = dwnurl.replace("/","_").replace(":__","/",1) + ".ipynb"
 #plan is to inject the url into a template and write it to fn, so a direct link2it can come from the search gui
-print(f'open:{fn}')
+#print(f'open:{fn}')
 #could check then open the NB if there, but might change template4a bit, so wait till settles
 
 #probably better to use: https://github.com/nteract/papermill to inject the url, but maybe not, bc want above naming
@@ -17,7 +17,6 @@ base_url = "http://141.142.218.86:8081/notebooks/"
 
 def pm(dwnurl, fn):
     import papermill as pm
-
     pm.execute_notebook(
        'mybinder-read-pre-gist.ipynb', #path/to/input.ipynb',
        fn,  #'path/to/output.ipynb',
@@ -25,7 +24,7 @@ def pm(dwnurl, fn):
     )
     return base_url + fn
 
-    #above had problems, so try cli too:
+    #above had problems(on1machine), so have cli backup in case:
 def pm2(dwnurl, fn):
     import os
     cs=f'papermill mybinder-read-pre-gist.ipynb {fn} -p url {dwnurl}'
@@ -37,9 +36,26 @@ def pm2(dwnurl, fn):
 # will also need to be able to find this gist given the dwnlink, to not reimpliment, so might have2do a gist search,
 # but exmpl already gives a listing of all of them, so look for the fn part&cmp
 #via, some setup then:
-#txt = Path(fn).read_text()
-#payload={"description":"GIST created by python code","public":True,"files":{fn_:{"content":txt}}}
-#res=requests.post(url,headers=headers,params=params,data=json.dumps(payload))
+def post_gist(fn):
+    import requests
+    import json
+    import os
+    from pathlib import Path
+    GITHUB_API="https://api.github.com"
+    API_TOKEN=os.getenv('gist_token')
+    url=GITHUB_API+"/gists"
+    headers={'Authorization':'token %s'%API_TOKEN}
+    params={'scope':'gist'}
+    txt = Path(fn).read_text()  #should incl full output of papermil run w/dwnload url param
+    fn_=fn.replace("https/","") #filepath2filename only  #also4 http only  #might change description 
+    payload={"description":"GIST created by py code","public":True,"files":{fn_:{"content":txt}}}
+    res=requests.post(url,headers=headers,params=params,data=json.dumps(payload))
+    #also a way2go through present gists, if had2find if it already exists
+    return res #might want to(instead)ret&save gist url
+
+def tpg(fn="https/darchive.mblwhoilibrary.org_bitstream_1912_26532_1_dataset-752737_bergen-mesohux-2017-dissolved-nutrients__v1.tsv.ipynb"): #test
+    r=post_gist(fn)
+    print(r)
 
 #works in tp2.py
 #pm2(dwnurl, fn)
@@ -48,11 +64,10 @@ from flask import Flask
 from markupsafe import escape
 app = Flask(__name__)
 
-
 @app.route('/mknb/<dwnurl>')
 def mknb(qry):
     "grep jsonld, get as html"
     dwnurl_str=escape(dwnurl)
-    fn = dwnurl_str.replace("/","_").replace(":__","/",1) + ".ipynb"
-    r=pm(dwnurl_str, fn)
+    fn = dwnurl_str.replace("/","_").replace(":__","/",1) + ".ipynb"  #dwnloadURL2filePath it's saved in, ;almost as sep fnc, 
+    r=pm(dwnurl_str, fn)                                                #then could call from pm-fncs &only take 1arg
     return r
