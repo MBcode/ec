9590b6eabe02ff0625405876c407c0af487615b7
Mike Bobak <mbobak@illinois.edu>
2021-07-21 19:35:51 -0500

    read w/diff ext passed in

--- NoteBook/read_file.py
+++ NoteBook/read_file.py
@@ -2,16 +2,18 @@ def read_file(fn, ext=None):
     "can be a url, will call pd read_.. for the ext type"
     import pandas as pd
     import os
+    import re
     if(ext!=None):
         ft="." + ext
     else:
         st=os.path.splitext(fn)
         ft=st[-1]
-    if ft=='.tsv':
+    if ft=='.tsv' or re.search('.tsv',ext):
         df=pd.read_csv(fn, sep='\t')
-    elif ft=='.csv':
+    elif ft=='.csv' or re.search('csv',ext):
         df=pd.read_csv(fn)
-    elif ft=='.txt':
+    #elif ft=='.txt' or ext=='text/plain':
+    elif ft=='.txt' or re.search('text',ext):
         df=pd.read_csv(fn, sep='\n')
     else:
         df="no reader, can !wget the file"
