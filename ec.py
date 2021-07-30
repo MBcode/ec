a4bed9648542dbc62e7599f84c4bdc8515a672e0
Mike Bobak <mbobak@illinois.edu>
2021-07-30 15:51:52 -0500

    if can note pd.read.. wget and if fileExt mv to create1

--- ec.py
+++ ec.py
@@ -1,29 +1,59 @@
-def read_file(fnp, ext=None):
-    "can be a url, will call pd read_.. for the ext type"
-    import pandas as pd
-    import os
-    import re
-    fn=fnp.strip('/')
-    url = fn
-    if(ext!=None):
-        ft="." + ext
-    else:
-        st=os.path.splitext(fn)
-        ft=st[-1]
-    if ft=='.tsv' or re.search('tsv',ext,re.IGNORECASE) or re.search('tab-sep',ext,re.IGNORECASE):
-        df=pd.read_csv(fn, sep='\t',comment='#')
-    elif ft=='.csv' or re.search('csv',ext,re.IGNORECASE):
-        df=pd.read_csv(fn)
-    elif ft=='.txt' or re.search('text',ext,re.IGNORECASE):
-        df=pd.read_csv(fn, sep='\n',comment='#')
-#   elif ft=='.zip' or re.search('zip',ext,re.IGNORECASE):
-#       df=pd.read_csv(fn, sep='\t',comment='#')
-    else:
-        df="no reader, can !wget $url"
-    return df
+#pagemil parameterized colab/gist can get this code via:
+#with httpimport.github_repo('MBcode', 'ec'):
+#  import ec
+import os
 
 #start adding more utils, can use to: fn=read_file.path_leaf(url) then: !head fn
 def path_leaf(path):
     import ntpath
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
+
+def file_ext(fn):
+    st=os.path.splitext(fn)
+    return st[-1]
+
+def wget(fn):
+    cs= f'wget {fn}' 
+    os.system(cs)
+
+def add_ext(fn,ft):
+    fn1=path_leaf(fn) #just the file, not it's path
+    fext=file_ext(fn1) #&just it's .ext
+    if fext==None or fext=='':
+        fnt=fn1 + ft
+        cs= f'mv {fn1} {fnt}' 
+        os.system(cs)
+
+def wget_ft(fn,ft):
+    wget(fn)
+    add_ext(fn,ft)
+
+def read_file(fnp, ext=None):
+    "can be a url, will call pd read_.. for the ext type"
+    import pandas as pd
+    import re
+    fn=fnp.strip('/')
+    fn1=path_leaf(fn) #just the file, not it's path
+    fext=file_ext(fn1) #&just it's .ext
+    #url = fn
+    if(ext!=None):
+        ft="." + ext
+    else: #use ext from fn
+        ft=fext
+    df=""
+    if ft=='.tsv' or re.search('tsv',ext,re.IGNORECASE) or re.search('tab-sep',ext,re.IGNORECASE):
+        df=pd.read_csv(fn, sep='\t',comment='#')
+    elif ft=='.csv' or re.search('csv',ext,re.IGNORECASE):
+        df=pd.read_csv(fn)
+    elif ft=='.txt' or re.search('text',ext,re.IGNORECASE):
+        df=pd.read_csv(fn, sep='\n',comment='#')
+    elif ft=='.zip' or re.search('zip',ext,re.IGNORECASE):
+        ft='.zip'
+        wget_ft(fn,ft)
+#       df=pd.read_csv(fn, sep='\t',comment='#')
+    else:
+        wget_ft(fn,ft)
+        #df="no reader, can !wget $url"
+        df="no reader, doing:[!wget $url ],to see:[ !ls -l ]"
+    return df
