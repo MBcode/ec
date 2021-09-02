0efbf607c7e311afdd6a04d92fa78049358f3305
mike bobak <mike.bobak+git@gmail.com>
2021-09-02 13:59:14 -0500

    fix else:

--- ec.py
+++ ec.py
@@ -302,7 +302,7 @@ def read_file(fnp, ext=None):
         #if ft is blank, can: grep -A4 $url *.nt|grep encoding|cut -d' ' -f3
         if ft and len(ft)<2:
             ext=nt2ft(fnp) #but put in 'ext' bc it does the re. of the longer txt from the .nt file
-        else
+        else:
             ext=ft
     df=""
     if ext==None and len(ft)<1:
