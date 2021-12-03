7f65c3cd9eb54773a3fd3d575a199e91872a889d
mike bobak <mike.bobak+git@gmail.com>
2021-12-03 16:24:49 -0600

    show_related strip descriptions

--- ec.py
+++ ec.py
@@ -810,9 +810,9 @@ def test_related(q,row=0): #eg "Norway"
 #but sparql-nd will already have the df calculated, so just do the similarity-matrix for it once, 
  #then call get_related_indices for each dataset/row you want to look at, or can now use:
 def show_related(df,row):  #after dfCombineFeaturesSimilary is run on your df 'sparql results'
-    main=df['description'][row]
+    main=df['description'][row].strip()
     print(f'related to row={row},{main}')
     related=get_related_indices(row)
     for ri in related:
-        des=df['description'][ri]
+        des=df['description'][ri].strip()
         print(f'{ri}:{des}')
