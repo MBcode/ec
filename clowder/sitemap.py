fc3382e94956a40fbc98add1f90d05722d8d71a1
m bobak <mbobak@illinois.edu>
2022-05-04 13:15:43 -0500

    cli arg w/url for datasets

--- clowder/sitemap.py
+++ clowder/sitemap.py
@@ -1,32 +1,31 @@
 #generate a sitemap for all the datasets, via the clowder api
+# http://isda.ncsa.uiuc.edu/~mbobak
 import requests
 import json
+import sys
 top= """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    """ 
 
-#do this w/in the loop, next
+#could do this w/in the loop
 def put_txtfile(fn,s,wa="w"):
-    #with open(fn, "w") as f:
     with open(fn, "a") as f:
         return f.write(s)
 
-sm = "sitemap.xml"
-url = "http://localhost:9000/api/datasets" #have as cli arg next
-#URLb = "http://localhost:9000/" #same as:
-URLb = url.strip("api/datasets")
+def datasetlist2sitemap(url,sm="sitemap.xml"):
+    r=requests.get(url)
+    put_txtfile(sm,top)
+    URLb = url.strip("api/datasets")
+    ja=json.loads(r.text)
+    for ds in ja:
+        id=ds.get('id')
+        if(id):
+            put_txtfile(sm,f'<url><loc>{URLb}/datasets/{id}</loc></url>')
 
-r=requests.get(url)
-#print(f'r={r}')
-#print(f'txt={r.text}')
-#print(f'url={r.url}')
-
-#print(top)
-put_txtfile(sm,top)
-
-ja=json.loads(r.text)
-
-for ds in ja:
-    id=ds.get('id')
-    if(id):
-        put_txtfile(sm,f'<url><loc>{URLb}/datasets/{id}</loc></url>')
+if __name__ == '__main__':
+    #if >2 could set alt sitemap name
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+    else:
+        url = "http://localhost:9000/api/datasets" 
+    datasetlist2sitemap(url)
