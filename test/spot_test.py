ab9412366d8ff12b853c6ddc67434f094816a779
m bobak <mbobak@illinois.edu>
2022-09-07 12:52:08 -0500

    start2reformat for spot_report

--- test/spot_test.py
+++ test/spot_test.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 #m bobak, run gleaner+nabu then check the values against expected
+#add timing for mockup, and expand final dropoff printout to be multiline like in
+# https://github.com/earthcube/geocodes_documentation/wiki/DataValidationReportMockup
+#will split out lines like this: print('sitemap-count:{sml}, Error count:{ls1}, missing:{loose_s2s}')
+# from present compact return format: dropoff=f'sitemap:{sml}-{lsl}:{lose_s2s} =>{dropoff2}'
 cfg="mb_ci"
 import ec
 ec.local()
@@ -45,11 +49,15 @@ def r2():
     print(report2) 
 
 def all_new():
+    start=ec.now()
+    print(f'Start:{start}')
     lb()
     new_run()
     lb()
     r1()
     lb()
     r2()
+    end=ec.now()
+    print(f'End:{end}') #get w/elapse Runtime, instead
 
-all_new()
+#all_new()
