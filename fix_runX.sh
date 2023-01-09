89812061b4c85b9f63b341a023754e9902819b6e
m bobak <mbobak@illinois.edu>
2023-01-09 09:18:44 -0600

    get/fix crawl of a repo so can summarize_repo.sh next

---
+++ fix_runX.sh
@@ -0,0 +1,3 @@
+#to resolve https://github.com/gleanerio/gleaner/issues/126 till gleaner can dump quads in runX
+run2nq.py $1 
+#once gleaner can dump quads, we will just pull (v fix) them&run: summarize_repo.sh next 
