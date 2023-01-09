6a90eaa2bd0ba1536fb577d37d12ca64fec9d6a5
m bobak <mbobak@illinois.edu>
2023-01-09 14:46:57 -0600

    update descriptive comments

--- summary/repo2summary.sh
+++ summary/repo2summary.sh
@@ -1,5 +1,5 @@
 #to show that now it has to get into ./repo dir to fix the quads &cp up to ./repo.nq
 fix_runX.sh $1
-#takes repo.nq loads to fusekly /repo namespace, and runs tsum.py repo to make a repo.ttl file
+#takes repo.nq loads to fuseki /repo namespace, and runs: 'tsum.py repo' to make a repo.ttl file
 repo2summary.sh $1
-#later could have a nt2blaze.sh to put in the store
+#later could have a ttl2blaze.sh to put that in the store
