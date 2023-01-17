0a8592e4a2be2660c36daf05948613a20c2c60db
m bobak <mbobak@illinois.edu>
2023-01-17 09:48:49 -0600

    fix to be like call.md

--- summary/repo2summary.sh
+++ summary/repo2summary.sh
@@ -1,5 +1,5 @@
 #to show that now it has to get into ./repo dir to fix the quads &cp up to ./repo.nq
 fix_runX.sh $1
 #takes repo.nq loads to fuseki /repo namespace, and runs: 'tsum.py repo' to make a repo.ttl file
-repo2summary.sh $1
+summarize_repo.sh $1
 #later could have a ttl2blaze.sh to put that in the store
