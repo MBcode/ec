c97abe2f6e8a0bf2270eca9a0a437e8c383384a9
m bobak <mbobak@illinois.edu>
2023-03-07 15:53:17 -0600

    read crawl config to get values needed to get counts for a dashboard

---
+++ test/localConfigCounts.py
@@ -0,0 +1,83 @@
+#mbobak, parse crawl cfg, to get sitemap+graph to get counts from them
+import pandas as pd
+dbg=True
+
+#from mb.py part of utils, for now
+def path_leaf(path):
+    "everything after the last /"
+    import ntpath
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+# https://www.golinuxcloud.com/python-convert-yaml-file-to-dictionary/
+def yml2d(fn):
+    "yml file to dict"
+    import yaml
+    from yaml.loader import BaseLoader
+    # opening a file
+    with open(fn, 'r') as stream:
+        try:
+            # Converts yaml document to python object
+            d=yaml.load(stream, Loader=BaseLoader)
+            if dbg:
+                for key, val in d.items():
+                    print(key, " : ", val,"\n")       
+                return d
+        except yaml.YAMLError as e:
+            print(e)
+            return None
+
+def df_cols2dict(df,c1,c2):
+    "make a dict from 2 df cols"
+    #d={}
+    print(f'will pull out, c1={c1},c2={c2}')
+    d=df.set_index(c1).to_dict()[c2]
+    if dbg:
+        print(f'd={d}')
+    return d
+
+def parse_localConfig(fn='localConfig.yaml'):
+    "parse gleaner cfg, ret: name2url dict .."
+    d=yml2d(fn)
+    sgD=d['sourcesSource']
+    loc=sgD['location']
+    if dbg: #where the Sources csv is
+        print(f'loc={loc}')
+    df=pd.read_csv(loc)
+    if dbg:
+        print(df)
+    #really want to pull out repo-name&sitemap cols, to make a mapping dict
+    d=df_cols2dict(df,"Name","URL")
+    #d={'balto': 'http://balto.opendap.org/opendap/site_map.txt ' ...
+    return d
+
+def parse_nabu(fn='nabu'):
+    "get prefix=sites crawled,&endpoint"
+    d=yml2d(fn)
+    obD=d['objects']
+    reposP=obD['prefix']
+    print(f'reposP={reposP}')
+    #reposP=['summoned/opentopography', 'summoned/iris', 'summoned/hydroshare', 'summoned/iedadata', 'summoned/opentopography', 'summoned/iris', 'summoned/magic', 'summoned/earthchem', 'summoned/usap-dc']
+    repos=list(map(path_leaf,reposP))
+    print(f'repos={repos}')
+    #could use this Path version for intermediate summoned counts next; see ec testing file
+    sD=d['sparql']
+    endpoint=sD['endpoint']
+    #endpoint=https://graph.geocodes.ncsa.illinois.edu/blazegraph/namespace/earthcube2/sparql
+    print(f'endpoint={endpoint}')
+    return repos, endpoint
+
+def crawl_cfg2counts():
+    "use crawl cfg to pull out counts: sitemaps+graph"
+    repo2url = parse_localConfig()
+    repos, endpoint = parse_nabu()
+    #then map repo2url on repos -> sitemaps
+    for repo in repos:
+        url=repo2url[repo]
+        print(f'url={url}')
+    #map ec sitemap_counts over that -> output1 
+    #ec graph_counts as 2nd part of final jina2/streamlit template
+
+def t1():
+    "test:could pass through lc&nabu file names if they change"
+    crawl_cfg2counts()
